4b016b83f125d02725d5ea752e925c94c5e5b37f
Mohammedadn <mohammedaden023@gmail.com>
2024-04-26 20:49:36 +0300

    firddd

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
+import React from 'react';
+
 
 function Keypad (){
+    function passWordChange(){
+        console.log('Entering password...');
+    }
+
     return (
-        <div></div>
+        <div>
+
+            <input type="password" onChange={passWordChange} />
+
+        </div>
     )
 }
 
